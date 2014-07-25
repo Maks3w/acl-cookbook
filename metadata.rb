382bbfb0596e2f80165974cba29232fa6fc96e17
Maks3w <github.maks3w@virtualplanets.net>
2014-07-25 22:41:43 +0200

    First stable release

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Maks3w'
 license 'MIT'
 description 'Installs/Configures acl'
 long_description 'Setup POSIX Access Control Lists (ACLs)'
-version '0.1.0'
+version '1.0.0'
 
 %w(ubuntu debian).each do |os|
   supports os
