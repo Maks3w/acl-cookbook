548992afb7e44766e6084ec0bb87dc061e85696c
Maks3w <github.maks3w@virtualplanets.net>
2014-08-22 18:48:35 +0200

    [1.0.2] Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,15 @@
+# 1.0.2
+
+Update CHANGELOG.md with the changes on previous releases
+
+# 1.0.1
+
+Don't exclude CHANGELOG.md from the tarball
+
+# 1.0.0
+
+First stable version
+
 # 0.1.0
 
 Initial release of acl
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Maks3w'
 license 'MIT'
 description 'Setup POSIX Access Control Lists (ACLs)'
 long_description 'Setup POSIX Access Control Lists (ACLs)'
-version '1.0.1'
+version '1.0.2'
 
 %w(ubuntu debian).each do |os|
   supports os
